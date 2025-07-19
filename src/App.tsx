83cc80977fab6551ffec96c57b2c44ffb8d2367a
joseph <rkfnahs12e@gmail.com>
2025-07-19 21:16:55 +0900

    test : CalendarHeader test 로직 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import Calendar from '@/widgets/calendar/ui/Calendar';
+import Calendar from './widgets/calendar/ui/Calendar';
 
 function App() {
   return (
-    <div className='w-screen h-screen p-8'>
+    <div>
       <Calendar />
     </div>
   );
