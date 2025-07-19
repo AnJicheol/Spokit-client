6ad04100021bb5bd047ae37d1250e309acd48b1b
joseph <rkfnahs12e@gmail.com>
2025-07-19 20:53:53 +0900

    test : CalendarNav 테스트 작성

--- src/widgets/calendar/ui/CalendarNav.tsx
+++ src/widgets/calendar/ui/CalendarNav.tsx
@@ -1,9 +1,7 @@
 import { addMonths, subMonths, addWeeks, subWeeks } from 'date-fns';
 import { type viewMode } from '../model';
-import LeftIcon from '@/assets/left.svg?react';
-import RightIcon from '@/assets/right.svg?react';
 import { useCallback } from 'react';
-
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 interface CalendarNavProps {
   baseDate: Date;
   setBaseDate: (date: Date) => void;
@@ -44,12 +42,6 @@ export default function CalendarNav({ baseDate, setBaseDate, mode }: CalendarNav
   }, [setBaseDate]);
   return (
     <div className='flex-1 h-full flex gap-2 justify-end items-end'>
-      <div
-        onClick={handlePrev}
-        className='embossed w-8 h-8 bg-cal-btn-bg rounded-full flex items-center justify-center cursor-pointer'
-      >
-        <LeftIcon className='w-[18px] h-[18px]' />
-      </div>
       <button
         onClick={handleTodayClick}
         className='embossed bg-cal-btn-bg hover:bg-cal-btn-bg cursor-pointer px-[20.5px] py-1 rounded-[10px] flex items-center justify-center'
@@ -62,10 +54,18 @@ export default function CalendarNav({ baseDate, setBaseDate, mode }: CalendarNav
         </span>
       </button>
       <div
-        onClick={handleNext}
-        className='embossed w-8 h-8 bg-cal-btn-bg rounded-full flex items-center justify-center cursor-pointer '
+        data-testid='cal-nav-prev-button'
+        onClick={handlePrev}
+        className='embossed p-2 bg-cal-btn-bg rounded-full flex items-center justify-center cursor-pointer'
       >
-        <RightIcon className='w-[18px] h-[18px]' />
+        <ChevronLeft size={18} color='white' />
+      </div>
+      <div
+        data-testid='cal-nav-next-button'
+        onClick={handleNext}
+        className='embossed p-2 bg-cal-btn-bg rounded-full flex items-center justify-center cursor-pointer '
+      >
+        <ChevronRight size={18} color='white' />
       </div>
     </div>
   );
